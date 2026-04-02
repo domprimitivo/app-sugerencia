3ea02dc8c5ff786ea5fff6fbb5fc494f9494e1cb
httpatz <http.atz@gmail.com>
2026-04-02 10:03:26 -0600

    F-02: reemplazar __init__ rag_agents con exportaciones reales

--- backend/aprendiz_motor/rag_agents/__init__.py
+++ backend/aprendiz_motor/rag_agents/__init__.py
@@ -1,14 +1,9 @@
-# RAG Agents - Paquete
-from .processor import (
-    RAGProcessor,
-    inicializar_rag,
-    procesar_expediente,
-    consultar_rag
-)
+"""
+aprendiz_motor/rag_agents/__init__.py
+Exporta la API pública que server.py llama:
+  from aprendiz_motor.rag_agents import inicializar_rag
+  from aprendiz_motor.rag_agents import procesar_expediente as rag_procesar
+"""
+from .processor import inicializar_rag, procesar_expediente
 
-__all__ = [
-    "RAGProcessor",
-    "inicializar_rag",
-    "procesar_expediente", 
-    "consultar_rag"
-]
+__all__ = ["inicializar_rag", "procesar_expediente"]
